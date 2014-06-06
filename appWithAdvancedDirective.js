44ad1d962ca09ac4a7d70099d56ca2ca18cc7ded
Mark Freedman <mark@markfreedman.com>
2014-06-06 00:22:59 -0400

    Updated the basic directive example, and created an advanced example.

--- appWithAdvancedDirective.js
+++ appWithAdvancedDirective.js
@@ -15,7 +15,7 @@ app.config(['$routeProvider', function ($routeProvider) {
         controller: 'StudentController'
     }).
     when('/students', {
-        templateUrl: 'partials/studentsWithDirective.html',
+        templateUrl: 'partials/studentsWithAdvancedDirective.html',
         controller: 'StudentController'
     }).
     otherwise({
@@ -94,28 +94,49 @@ app.controller("ClassroomController", function ($scope) {
 
 app.controller("StudentController", function ($scope, $routeParams, studentRepository) {
   $scope.students = studentRepository.getStudents();
-  $scope.chosenColor = "red";
 
   $scope.displayStudentInfo = function(student) {
     alert(student.name);
   }
 
-  $scope.changeColor = function(color) {
-      $scope.chosenColor = color;
-  }
-
   if ($routeParams.classroom) $scope.classroom = $routeParams.classroom;
 });
 
-app.directive("colorize", function () {
+
+app.directive("collapsible", function () {
   return {
-    restrict: "AE", // This directive can be used either as an attribute or as an element.
+    restrict: "E", // Only use as an element directive
     scope: {
-      color: "=" // Two-way binding will be used for the color.
+      title: "@" // One-way data-binding to the "title" attribute on the directive element to the isolated scope.
     },
-    template: "<span>" +
-                "<span style='color: {{color}};' data-ng-transclude></span>" +
-              "</span>",
-    transclude: true // Since we're wrapping some HTML, we want to inject it back into the template.
+    template: "<div>" +
+                "<h3 data-ng-click='toggleVisibility()'>{{title}}</h3>" +
+                "<div data-ng-show='visible' data-ng-transclude></div>" +
+              "</div>",
+    replace: true, // Completely replace the directive HTML with the resulting template.
+    link: function (scope, element, attrs, ctrls, transclude) {
+      // Used for preparing the DOM for the resulting template. Here, because transcluded HTML does not use isolated
+      // scope, AND because it gets a new COPY of the outside controller's scope, we can't do two way data binding
+      // on the data model used for filtering unless we "clone" the elements at link time. Also, we have to remove the
+      // originally transcluded HTML, otherwise we'll have a double occurance of the INPUT tag, with the original not
+      // working due to the scope issue mentioned above. So we need to remove that element first, then append the cloned
+      // version.
+      //
+      // I banged my head against this for days, and it's still not crystalized in my mind. When I finally do grok it,
+      // I'll update my comments.
+      transclude(scope.$parent, function(clone, scope) {
+        element.children().eq(1).empty();
+        element.children().append(clone);
+      });
+    },
+    controller: function ($scope) {
+      $scope.visible = true;
+
+      $scope.toggleVisibility = function () {
+        $scope.visible = !$scope.visible;
+        if (!$scope.visible) $scope.$parent.search.name = ""; // Reset the search string when turning off the filter.
+      };
+    },
+    transclude: true // Include the HTML wrapped inside the directive element into the resulting HTML.
   };
 });
